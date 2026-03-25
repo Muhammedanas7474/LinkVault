afe71e6b6e09f5861452a3dcfe4cdda3b2e8e8d7
anas <muhammedanas247474@gmail.com>
2026-03-25 14:50:24 +0530

    chore : project split(base/local/production)

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # LinkVault
+# Python
+__pycache__/
+*.pyc
+*.pyo
+*.pyd
+.env*
+venv/
+db.sqlite3
+
+# Django
+media/
+staticfiles/
+
+# Node (future frontend)
+node_modules/
+dist/
+
+# OS
+.DS_Store
+
+
---
+++ backend/core/settings/production.py
@@ -0,0 +1,34 @@
+from .base import  * 
+
+DEBUG = False 
+
+ALLOWED_HOSTS = [config('ALLOWED_HOST')]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+        'CONN_MAX_AGE': 60,
+    }
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+CORS_ALLOWED_ORIGINS = [config('FRONTEND_URL')]
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
